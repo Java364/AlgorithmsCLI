d7b1648e8d15e53ea19501d7124d7b03f55bec16
Igor Flyunt <igflyunt@gmail.com>
2018-11-12 10:51:17 +0200

    remove redundant method

--- src/main/java/com/softserve/cli/impl/SortCLI.java
+++ src/main/java/com/softserve/cli/impl/SortCLI.java
@@ -3,7 +3,6 @@ package com.softserve.cli.impl;
 import com.softserve.algorithms.sort.Sort;
 import com.softserve.cli.AbstractCLI;
 import com.softserve.cli.InvalidInputHandler;
-import com.softserve.cli.option.AlgorithmOption;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -25,9 +24,5 @@ public class SortCLI extends AbstractCLI {
 
     public void setSortStrategy(Sort sortStrategy) {
         this.sortStrategy = sortStrategy;
-    }
-
-    public void setAlgorithmOption(AlgorithmOption algorithmOption) {
-        this.algorithmOption = algorithmOption;
     }
 }
