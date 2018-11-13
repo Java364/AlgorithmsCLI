601788245d690f95446299393607faedb4964ef5
Igor Flyunt <igflyunt@gmail.com>
2018-11-14 00:29:07 +0200

    clean up testing code

--- src/main/java/com/softserve/Main.java
+++ src/main/java/com/softserve/Main.java
@@ -1,25 +1,14 @@
 package com.softserve;
 
-import com.softserve.cli.AbstractCLI;
-import com.softserve.cli.impl.FibonacciCLI;
-import com.softserve.cli.impl.LongestSubsequenceCLI;
 import com.softserve.cli.impl.MainCommandLineInterface;
-import com.softserve.cli.option.CommonAlgorithmOption;
 
-import java.io.IOException;
 import java.util.Scanner;
-import java.util.function.BooleanSupplier;
 
 public class Main {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            final AbstractCLI fibonacciCLI = new LongestSubsequenceCLI(scanner);
-            fibonacciCLI.setAlgorithmOption(CommonAlgorithmOption.LONGEST_SUBSEQUENCE);
-            fibonacciCLI.run();
+            new MainCommandLineInterface(scanner).run();
         }
     }
 
-    public static boolean test() {
-        throw new RuntimeException();
-    }
 }
