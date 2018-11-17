3074f99e79cba8f35c5bcdfa60f723673da86c08
KatyaKaratnyk <k.karatnik@meta.ua>
2018-11-17 19:30:08 +0200

    add throws IllegalArgument Exeption

--- src/main/java/com/softserve/algorithms/WaysToSumUsingArray.java
+++ src/main/java/com/softserve/algorithms/WaysToSumUsingArray.java
@@ -2,8 +2,18 @@ package com.softserve.algorithms;
 
 public class WaysToSumUsingArray {
 	
-	public int getAllConbinationsOfSum(int sequence[], int sum) { 
-        int count[] = new int[sum + 1]; 
+	public int getAllConbinationsOfSum(int sequence[], int sum) throws IllegalArgumentException{ 
+        
+		if(sum <= 0) {
+			throw new IllegalArgumentException("sum cann't be negative or equel zero");
+		}
+		for(int e: sequence) {
+			if(e < 0) {
+				throw new IllegalArgumentException("number of sequence cann't be negative");
+			}
+		}
+		
+		int count[] = new int[sum + 1]; 
         count[0] = 1; 
         for (int i = 1; i <= sum; i++) {
             for (int j = 0; j < sequence.length; j++) {
