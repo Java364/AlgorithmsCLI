7089bbf0aa0ad6cb0f527bf1178f2dc48fa5a03d
Igor Flyunt <igflyunt@gmail.com>
2018-11-18 13:24:36 +0200

    update fibonacci algorithm

--- src/main/java/com/softserve/cli/impl/FibonacciCLI.java
+++ src/main/java/com/softserve/cli/impl/FibonacciCLI.java
@@ -9,7 +9,8 @@ import com.softserve.cli.util.InputUtil;
 import java.util.Scanner;
 
 public class FibonacciCLI extends AbstractCLI implements CommandLineInterface {
-    private int fibonacciPos = 0;
+    private int fibonacciPos  = 0;
+    private int fibonacciStep = 0;
 
     public FibonacciCLI(Scanner prompt) {
         super(prompt);
@@ -17,18 +18,27 @@ public class FibonacciCLI extends AbstractCLI implements CommandLineInterface {
 
     @Override
     public void run() {
-        System.out.printf("%s\n%s\n", algorithmOption.getOptionName(),
-                          algorithmOption.getOptionDescription());
+        System.out.printf("%s\n%s\n", algorithmOption.getOptionName(), algorithmOption.getOptionDescription());
         InvalidInputHandler.promptIfInvalidValue(this::promptFibonacciNumber, INVALID_NUMBER_MSG);
-        int fibResult = Algorithms.findFibonacciMember(fibonacciPos);
-        System.out.format("Fibonacci member at position %d is %d\n", fibonacciPos, fibResult);
+        InvalidInputHandler.promptIfInvalidValue(this::promptFibonacciStep, INVALID_NUMBER_MSG);
+        int fibResult = Algorithms.findFibonacciMember(fibonacciPos, fibonacciStep);
+        System.out.format("Fibonacci member at position %d with step %d is %d\n",
+                          fibonacciPos, fibonacciStep, fibResult);
     }
 
     private boolean promptFibonacciNumber() {
-        System.out.print("Please enter findFibonacciMember position(only positive integer numbers): ");
+        System.out.print("Please enter fibonacci member position(only positive integer numbers): ");
         if (!InputUtil.hasNextInt(prompt))
             return false;
-        this.fibonacciPos = InputUtil.readIntValue(prompt);
-        return this.fibonacciPos >= 1;
+        fibonacciPos = InputUtil.readIntValue(prompt);
+        return fibonacciPos >= 0;
+    }
+
+    private boolean promptFibonacciStep() {
+        System.out.print("Please enter step (0 or 1 only): ");
+        if (!InputUtil.hasNextInt(prompt))
+            return false;
+        fibonacciStep = InputUtil.readIntValue(prompt);
+        return (fibonacciStep == 0) || (fibonacciStep == 1);
     }
 }
